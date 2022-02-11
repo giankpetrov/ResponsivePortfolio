ec35bee176af34ac38202ab677f708378032c2eb
giankpetrov <giank7@gmail.com>
2022-02-11 01:02:05 +0000

    button menu animation html css js

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-const hamburgerButton = document.getElementById('hamburger')
+const menuBtn = document.querySelector('.hamburger-menu');
 const navList = document.getElementById('nav-list')
-
-function toggleButton() {
-        navList.classList.toggle('show')
-}
-
-hamburgerButton.addEventListener('click', toggleButton)
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+        if (!menuOpen){
+                menuBtn.classList.add('open');
+                menuOpen = true;
+                navList.classList.add('show');
+        } else {
+                menuBtn.classList.remove('open');
+                menuOpen = false;
+                navList.classList.remove('show');
+        }
+})
